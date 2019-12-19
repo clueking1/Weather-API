ef4ecf09f0560e0b60d69c2aecf977a744087f49
Will Coan <wcoan300@gmail.com>
2019-12-19 16:00:11 -0500

    final html

--- script/script.js
+++ script/script.js
@@ -1,14 +1,11 @@
+
+
 var citySearch = document.querySelector(".citySearch")
-var currentdate = new Date();
-var date = currentdate.getDate() 
-console.log(date)
-var dateArray = []
-
-
-
 citySearch.addEventListener("click", function(e){
         e.preventDefault()
         var city = $(".cityInput").val()
+        var currentdate = new Date();
+        var date = currentdate.getMonth()+1 + "-" + currentdate.getDate() 
         var lato = ""
         var lono = ""
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&units=imperial&APPID=654c88e2f3602b7e34cbe1a0f99b9ef0"
@@ -26,7 +23,10 @@ citySearch.addEventListener("click", function(e){
             var citydiv = $("<div></div>").addClass("city")
             var cityd = citydiv.append(cityName)
             $(".weatherPresent").append(cityd) 
-            
+
+            var currentdateDive = $("<div></div>").addClass("date").text(date)
+            $(".weatherPresent").append(currentdateDive) 
+
             var iconcode = response.weather[0].icon
             var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
             
@@ -88,7 +88,7 @@ function uv (a, b) {
 
 function day5weather (a) {
     var day5 = "http://api.openweathermap.org/data/2.5/forecast?q=" + a + ",us&units=imperial&APPID=654c88e2f3602b7e34cbe1a0f99b9ef0"
-    var dateArray = [{}]
+    var dateArray = []
     $.ajax({
         url: day5,
         method: "GET"
@@ -96,15 +96,40 @@ function day5weather (a) {
         console.log(response)
         for (i=0; i<40; i++) {
             timeOfDay = response.list[i].dt_txt
-            // console.log(timeOfDay)
+            //console.log(timeOfDay)
             if (timeOfDay.indexOf("12:00:00") !== -1) {
+                dateArray.push(i)
+                console.log(dateArray)
                 var days = response.list[i]
                 console.log(response.list[i])
+
+                var strSplit1 = timeOfDay.split(" ")
+                console.log(strSplit1)
+                var strSplit2 = strSplit1[0].split("-")
+                console.log(strSplit2)
+
+                var dates = strSplit2[1] + "-" + strSplit2[2]
+
+                var datesDiv = $("<div></div>").addClass("date5").text(dates)
+                $(".weatherFuture").append(datesDiv)
+ 
                 var iconcode = days.weather[0].icon
                 var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
             
                 var iconimg = $("<img>").attr("src", iconurl)
                 $(".weatherFuture").append(iconimg) 
+
+                var tempurdiv = $("<div></div>").addClass("tempNum5").text(days.main.temp + " ℉")
+                var humiddiv = $("<div></div>").addClass("humidNum5").text(days.main.humidity + "%")
+
+                var tempurdivHead = $("<div></div>").addClass("weatherHead5").text("Temp")
+                var humiddivHead = $("<div></div>").addClass("weatherHead5").text("Humidity")
+
+                $(".weatherFuture").append(tempurdivHead)
+                $(".weatherFuture").append(tempurdiv)
+                $(".weatherFuture").append(humiddivHead) 
+                $(".weatherFuture").append(humiddiv) 
+
             }
         }
 
