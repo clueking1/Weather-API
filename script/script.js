3d11839c97112b4aece2ad3d70a28efec455fead
Will Coan <wcoan300@gmail.com>
2019-12-30 02:13:52 -0500

    chnaged style to .css

--- script/script.js
+++ script/script.js
@@ -246,12 +246,12 @@ function showSlides(n) {
 }
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+      $(slides[i]).css("display", "none")
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  $(slides[slideIndex-1]).css("display", "block")
   dots[slideIndex-1].className += " active";
 }
 
